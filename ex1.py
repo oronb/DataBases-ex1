349b4dc1591ad202269707c1150345d942745539
oronBortman <oron.bortman@gmail.com>
2020-11-26 19:41:23 +0200

    Changes

--- ex1.py
+++ ex1.py
@@ -53,8 +53,15 @@ def check_valid_table(string):
 # last_par =  [end of string]
 
 
+
+"""
+OLD COND CODE WHICH CHECKS FOR MAIN LOGICAL OPERATOR AND COUNTS PARENTHESIS
+
 def check_cond(string):
     string = string.strip()
+    
+    # (a)
+    # NOT: (a) and (b)
     if cond_has_outer_parenthesis(string):
         string = string[1:-1]  # strip the outer parenthesis and re-check the trimmed string
         return check_cond(string)
@@ -152,7 +159,177 @@ def check_valid_parenthesis(string):
         return True
     else:
         return False
+"""
 
+"""
+def check_cond(string):
+    stripped_string = string.strip()
+    if stripped_string[0] == ')':
+        return False
+    if stripped_string[-1] == '(':
+        return False
+    if stripped_string[0] == '(':
+        # one recursion without removing ending parenthesis # GOOD FOR: (x and y) or z
+        if cond_rec_operator_break(stripped_string):
+            return True
+        # one recursion with removing, good for (x and y)
+        if stripped_string[-1] == ')':
+            string_without_parenthesis = stripped_string[1:-1]
+            if cond_rec_operator_break(string_without_parenthesis):
+                return True
+        return False
+    else:
+        return cond_rec_operator_break(stripped_string)
+
+
+# ((X AND Y) AND (X AND Y)
+def cond_rec_operator_break(string):
+    last_ind = len(string) - 1
+    location_to_look_from = 0
+    while location_to_look_from <= last_ind:
+        and_ind = string.find("AND", location_to_look_from)
+        or_ind = string.find("OR", location_to_look_from)
+        if and_ind < 0 and or_ind < 0:  # no AND or OR operators in string
+            stripped_string = string.strip()
+            if stripped_string[0] == ')':
+                return False
+            if stripped_string[-1] == '(':
+                return False
+            if stripped_string[0] == '(':
+                if stripped_string[-1] == ')':
+                    string_without_parenthesis = stripped_string[1:-1]
+                    if check_cond(string_without_parenthesis):
+                        return True
+                    else:
+                        location_to_look_from = last_ind;
+                else:
+                    return False
+            else:
+                return check_simple_cond(stripped_string)
+        else:
+            # find first of the two operators:
+            if and_ind < or_ind:
+                if and_ind >= 0:
+                    first_operator_ind = and_ind
+                    ind_after_operator = and_ind + 3
+                else:
+                    first_operator_ind = or_ind
+                    ind_after_operator = or_ind + 2
+            else:
+                if or_ind >= 0:
+                    first_operator_ind = or_ind
+                    ind_after_operator = or_ind + 2
+                else:
+                    first_operator_ind = and_ind
+                    ind_after_operator = and_ind + 3
+            string_left_of_operator = string[:first_operator_ind]
+            string_right_of_operator = string[ind_after_operator:]
+            left_res = check_cond(string_left_of_operator)
+            if left_res: # check right only if left is good - no point checking right otherwise
+                right_res = check_cond(string_right_of_operator)
+            if left_res and right_res:
+                return True
+            else:
+                location_to_look_from = ind_after_operator
+    return False
+"""
+
+def check_cond(string):
+    string = string.strip()
+    if cond_has_outer_parenthesis(string):
+        string = string[1:-1]  # strip the outer parenthesis and re-check the trimmed string
+        return check_cond(string)
+    else:
+        if check_valid_parenthesis(string):
+            ind_of_main_operator = find_main_operator_ind(string)
+            if ind_of_main_operator < 0:  # no logical operator AND or OR
+                return check_simple_cond(string)
+            else:
+                ind_after_main_operator = find_ind_after_operator(string, ind_of_main_operator)
+                left_string = string[:ind_of_main_operator]
+                right_string = string[ind_after_main_operator:]
+                return check_cond(left_string) and check_cond(right_string)
+        else:
+            return False
+
+
+def cond_has_outer_parenthesis(string):
+    stripped_string = string.strip()
+    return stripped_string[0] == '(' and stripped_string[-1] == ')'
+
+
+def find_main_operator_ind(string):
+    last_ind = len(string) - 1
+    location_to_look_from = 0
+    while location_to_look_from <= last_ind:
+        operator_ind = find_operator_ind(string, location_to_look_from)
+        if operator_ind < 0:  # no operator found from given location
+            return -1
+        else:
+            ind_after_operator = find_ind_after_operator(string, operator_ind)
+            string_left_of_operator = string[:operator_ind]
+            string_right_of_operator = string[ind_after_operator:]
+            if check_valid_parenthesis(string_left_of_operator) and check_valid_parenthesis(string_right_of_operator):
+                return operator_ind
+            else:
+                location_to_look_from = ind_after_operator
+
+
+def find_operator_ind(string, location_to_look_from):
+    and_ind = string.find("AND", location_to_look_from)
+    or_ind = string.find("OR", location_to_look_from)
+    if and_ind < 0 and or_ind < 0:  # no AND or OR operators in string from given index
+        return -1
+    else:
+        # find first of the two operators:
+        if and_ind < or_ind:
+            if and_ind >= 0:
+                first_operator_ind = and_ind
+                ind_after_operator = and_ind + 3
+            else:
+                first_operator_ind = or_ind
+                ind_after_operator = or_ind + 2
+        else:
+            if or_ind >= 0:
+                first_operator_ind = or_ind
+                ind_after_operator = or_ind + 2
+            else:
+                first_operator_ind = and_ind
+                ind_after_operator = and_ind + 3
+        return first_operator_ind
+
+
+def find_ind_after_operator(string, operator_ind):
+    if string[operator_ind] == 'A':
+        ind_after_operator = operator_ind + 3
+    else:  # operator is OR
+        ind_after_operator = operator_ind + 2
+    return ind_after_operator
+
+
+def check_valid_parenthesis(string):
+    stack = []
+    for i in string:
+        if i == '(':
+            stack.append(i)
+        elif i == ')':
+            if len(stack) > 0:
+                stack.pop()
+            else:
+                return False
+            """
+            pos = close_list.index(i)
+            if ((len(stack) > 0) and
+                    (open_list[pos] == stack[len(stack) - 1])):
+                stack.pop()
+            else:
+                return "Unbalanced"
+            """
+    if len(stack) == 0:
+        return True
+    else:
+        return False
+"""
 
 """
 def check_cond(string):
